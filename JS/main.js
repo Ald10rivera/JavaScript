5143e3924d32313930e09ee922183ce913ed23ab
Aldo Rivera <aldorivera507@gmail.com>
2022-07-25 22:03:09 -0300

    array y objetos

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,20 @@
-function convertir(){
-    let pesoarg = parseInt(document.getElementById("valor").value);
-    let resultado = 0;
-    let dolar = 130;
-    let chileno = 0.14
-    if(document.getElementById("uno").checked){
-        resultado = pesoarg * dolar;
-        alert("La conversion de pesos ARG a dolar es: $" + resultado);
-    }
-    if(document.getElementById("dos").checked){
-        resultado = pesoarg * chileno;
-        alert("La conversion de pesos ARG a CHIL es: $" + resultado);
-    }
-    else{
-        alert("No te olvides de completar todos los datos")
-    }
-}
+const listaComprasPredeterminada = [{
+    producto: "yogurt",
+    cantidad:10,
+    marca:"la serenisima",   
+},{producto: "queso",
+cantidad: "10kg",
+marca:"cremon", 
+},{producto: "pan",
+cantidad:"60kg",
+marca:"Bimbo", 
+}, {producto: "pre-pizzas",
+cantidad: 15,
+marca:"veganian", 
+}]
+
+let superM = parseInt (prompt("introduzca el numero correspondiente a la accion que desee \n1 lista predeterminada \n2 detallar lista \n3 salir"))
+if(superM === 1){ 
+    alert(listaComprasPredeterminada.length)
+    
+} 
