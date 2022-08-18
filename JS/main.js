d5caa5f2334d05af70f0f7037187decc17a51b75
Aldo Rivera <aldorivera507@gmail.com>
2022-08-17 23:01:45 -0300

    agregando libreria al proyecto

--- JS/main.js
+++ JS/main.js
@@ -1,101 +1,147 @@
-class Producto {
-    constructor(titulo, imagen, descripcion, precio){
-        this.titulo = titulo;
-        this.imagen = imagen;
-        this.descripcion = descripcion;
-        this.precio = precio;
+// Array de productos
+const productos = {
+    producto1: {
+    nombre: 'Iphone 11 Pro',
+    precio: '1200',
+    descripcion: 'El modelo cuenta con el nuevo chip Apple A13 Bionic y un nuevo sistema de cámara dual ultra ancho.1 Mientras que el iPhone 11 Pro presenta un nuevo cargador rápido de 18 vatios, el iPhone 11 presenta el mismo cargador de 5 vatios encontrado en iPhones anteriores.',
+    srcImg: './img/iphone.jpg'
+    },
+    producto2: {
+    nombre: 'Samsung S20 Plus',
+    precio: '1750',
+    descripcion: 'Un salto épico para la tecnología de smartphones. Un chip más rápido que nunca en Galaxy. El chip de 4 nm funciona con su poder de clase mundial en casi todos los aspectos de tu smartphone, fotos increíblemente claras de día y de noche y una experiencia de juego móvil optimizada para conducirte a la victoria.',
+    srcImg: './img/samsungs20.jpg'
+    },
+    producto3: {
+    nombre: 'Redmi Note 11 Pro',
+    precio: '980',
+    descripcion: ' El Xiaomi Redmi Note 10 Pro viene a mejorar el muy exitoso Redmi Note 9 Pro, el cual fue uno de los móviles estrellas del 2020 en cuanto a ventas se refiere. Entre sus principales cualidades, destaca un estupendo panel Amoled que hará las delicias de cualquiera y permitirá una correcta visualización incluso en las peores condiciones lumínicas.',
+    srcImg: './img/redmi.jpg'
+    },
+    producto4: {
+    nombre: 'Moto Edge 30 Plus',
+    precio: '1100',
+    descripcion: 'Motorola ha desvelado cuál será su apuesta internacional durante 2022: el Motorola Edge 30 Pro. Con la máxima potencia por bandera, y cargado de especificaciones en su interior, el móvil parte bien armado y con un precio que no se hace exagerado para el segmento premium.',
+    srcImg: './img/motog30.png'
     }
 }
-const producto1 = new Producto("Iphone 11 Pro", "./img/iphone.jpg","El modelo cuenta con el nuevo chip Apple A13 Bionic y un nuevo sistema de cámara dual ultra ancho.1 Mientras que el iPhone 11 Pro presenta un nuevo cargador rápido de 18 vatios, el iPhone 11 presenta el mismo cargador de 5 vatios encontrado en iPhones anteriores", 1200)
-const producto2 = new Producto("Samsung S20 Plus","./img/samsungs20.jpg","Un salto épico para la tecnología de smartphones. Un chip más rápido que nunca en Galaxy. El chip de 4 nm funciona con su poder de clase mundial en casi todos los aspectos de tu smartphone, fotos increíblemente claras de día y de noche y una experiencia de juego móvil optimizada para conducirte a la victoria.", "1750")
-const producto3 = new Producto("Redmi Note 11 Pro","./img/redmi.jpg"," El Xiaomi Redmi Note 10 Pro viene a mejorar el muy exitoso Redmi Note 9 Pro, el cual fue uno de los móviles estrellas del 2020 en cuanto a ventas se refiere. Entre sus principales cualidades, destaca un estupendo panel Amoled que hará las delicias de cualquiera y permitirá una correcta visualización incluso en las peores condiciones lumínicas","980")
-const producto4 = new Producto("Moto Edge 30 Plus","./img/motog30.png","Motorola ha desvelado cuál será su apuesta internacional durante 2022: el Motorola Edge 30 Pro. Con la máxima potencia por bandera, y cargado de especificaciones en su interior, el móvil parte bien armado y con un precio que no se hace exagerado para el segmento premium.","1100")
-
-const productos = []
-
-productos.push(producto1);
-productos.push(producto2);
-productos.push(producto3);
-productos.push(producto4);
-
-let carrito = []
-const moneda = '$'
-const DOMcarrito = document.querySelector('#carrito');
+  // Se captura el template de los productos
+    const templateProd = document.getElementById('template-prod').content
+    const contenedorProd = document.querySelector('.contenedor-productos')
+    const fragment = document.createDocumentFragment()
 
 
-function mostrarProductos(productos){
-
-    const contenedorDeProductos = document.getElementById("contenedor-de-productos");
-    contenedorDeProductos.innerHTML = "";
-
-    productos.forEach(producto => {
-        const divProducto = document.createElement("div");
-    divProducto.classList.add("producto");
-    divProducto.innerHTML = `
-    <img src="${producto.imagen}" alt="${producto.titulo}">
-    <h4>${producto.titulo}</h4>
-    <p>${producto.descripcion}</p>
-    <p>${producto.precio}</p>
-    `;
-    //=======
-    const botonComprar = document.createElement("button");
-    botonComprar.innerText = "comprar"
-    botonComprar.setAttribute('marcador',producto.titulo);
-    botonComprar.addEventListener("click",agregarCarrito);
-    divProducto.appendChild(botonComprar);
-    //=======
-    contenedorDeProductos.appendChild(divProducto);
+  // TODO LO RELACIONADO A AGREGAR LOS PRODUCTOS AL DOM
+    Object.values(productos).forEach( producto => {
+        templateProd.querySelector('.div-info .nombre-prod').textContent = producto.nombre
+        templateProd.querySelector('.div-precio-boton .precio').textContent = producto.precio
+        templateProd.querySelector('.div-info .descripcion-prod').textContent = producto.descripcion
+        templateProd.querySelector('.contenedor-img img').setAttribute('alt', producto.nombre)
+        templateProd.querySelector('.contenedor-img img').setAttribute('src', producto.srcImg)
+        const clone = templateProd.cloneNode(true)
+        fragment.appendChild(clone)
 })
-}
-/*funcion para agregar al carrito*/ 
-function agregarCarrito(evento){
+    contenedorProd.appendChild(fragment)
+
+  // TODO LO RELACIONADO AL CARRITO DE COMPRA
+    let carrito = {}
+    const templateTabla = document.getElementById('agregar-producto-al-carro').content
+    const tbodyCarrito = document.getElementById('carrito-body')
+    const fragmentTabla = document.createDocumentFragment()
+    const templateFoot = document.getElementById('tfooter').content
+    const tfootCarrito = document.getElementById('footer')
+
+    contenedorProd.addEventListener('click', e => {
     
-    carrito.push(evento.target.getAttribute('marcador'));
-    renderizarCarrito()
-    mostrarProductos(productos)
-
+if(e.target.textContent === "Agregar") {
+    setCarrito(e.target.parentElement.parentElement)
+    }
+    e.stopPropagation();
+})
+const setCarrito = e => {
+const pivoteCarrito = {
+    nombre: e.querySelector('.div-info .nombre-prod').textContent,
+    precio: e.querySelector('.div-precio-boton .precio').textContent,
+    cantidad: 1
+    }
+    if(carrito.hasOwnProperty(pivoteCarrito.nombre)){
+        carrito[pivoteCarrito.nombre].cantidad += 1
+    } else {
+        carrito[pivoteCarrito.nombre] = {...pivoteCarrito}
+    }
+    pintarTabla(carrito)
 }
-mostrarProductos(productos)
-console.log(carrito)
 
-//FUNCION PARA MOSTRAR EL CARRITO
-
-function renderizarCarrito(){
-    const carritoSinDuplicados = [...new set(carrito)];
-    carritoSinDuplicados.forEach((item) => {
-        const miItem = Producto.filter((itemProducto) =>{
-            return itemProducto.id === parseInt (item);
-        });
-        //contador de veces que se repite el producto 
-        const numeroUnidades = carrito.reduce((total, itemId)=>{
-            return itemId === item ? total += 1 : total;
-        },0);
-        //nodo del item carrito
-        const miNodo = document.createElement('li');
-        miNodo.textContent =`${numeroUnidades} x ${miItem[0].nombre} - ${miItem[0].precio}${moneda}`;
-
-        //boton de borrar
-        const miBoton = document.createElement('button');
-        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-            miBoton.textContent = 'X';
-            miBoton.style.marginLeft = '1rem';
-            miBoton.dataset.item = item;
-            miBoton.addEventListener('click', borrarItemCarrito);
-
-            miNodo.appendChild(miBoton);
-            DOMcarrito.appendChild(miNodo);
-
+const pintarTabla = objetoCarrito => {
+    Object.values(objetoCarrito).forEach( objeto => {
+        const cloneTabla = templateTabla.cloneNode(true)
+        cloneTabla.getElementById('producto').textContent = objeto.nombre
+        cloneTabla.getElementById('cant').textContent = objeto.cantidad
+        cloneTabla.getElementById('precio-uni').textContent = objeto.precio
+      let precioTotal = parseFloat(objeto.precio) * objeto.cantidad
+        cloneTabla.getElementById('precio-total-prod').textContent = precioTotal.toFixed(2)
+        fragmentTabla.appendChild(cloneTabla)
     })
+    tbodyCarrito.innerHTML = ''
+    tbodyCarrito.appendChild(fragmentTabla)
+    pintarFooter()
 }
+    const pintarFooter = () => {
+    tfootCarrito.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+        tfootCarrito.innerHTML = '<tr><td colspan = 4>¡No hay ningun elemento en el carrito!</td></tr>'
+    } else {
+      const total = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + (cantidad * precio),0)
+        templateFoot.getElementById('total-a-pagar').textContent = total.toFixed(2)
+        const cloneFoot = templateFoot.cloneNode(true)
+        fragment.appendChild(cloneFoot)
+        tfootCarrito.appendChild(fragment)
+//Boton Vaciar carrito
 
-function borrarItemCarrito(evento) {
+        const botonVaciar = document.getElementById('vaciar-tabla')
+    botonVaciar.addEventListener('click', () => {
+        carrito = {}
+        pintarTabla(carrito)
+        pintarFooter()
+        })
 
-    const id = evento.target.dataset.item;
-
-    carrito = carrito.filter((carritoId) => {
-        return carritoId !== id;
-    });
+      //Botones aumentar y disminuir cantidades
+    }
+}
+    tbodyCarrito.addEventListener('click', e => {
     
-    renderizarCarrito();
+    if(e.target.classList.contains('button')) {
+        aumentarDisminuir(e.target)
+    }
+})
+    const aumentarDisminuir = boton => {
+    if(boton.textContent === '+'){
+        const indicador = boton.parentElement.parentElement.firstElementChild.textContent
+        Object.values(carrito).forEach( elemento => {
+        if(elemento.nombre === indicador) {
+        carrito[elemento.nombre].cantidad++  
+        }
+    })
+    }
+    if(boton.textContent === '-') {
+        const indicador = boton.parentElement.parentElement.firstElementChild.textContent
+        Object.values(carrito).forEach( elemento => {
+        if(elemento.nombre === indicador) {
+        carrito[elemento.nombre].cantidad--
+            if(carrito[elemento.nombre].cantidad === 0) {
+            delete carrito[elemento.nombre]
+            }
+        }
+    })
+    }
+    pintarTabla(carrito)
+    pintarFooter()
 }
-
+    const botonFinalizarCompra = document.getElementById('botonFinalizar')
+    botonFinalizarCompra.onclick = () => {
+        Swal.fire(
+    'Genial, tu compra ha finalizado!',
+    'Gracias por elegirnos!',
+    'success'
+)
+    }
